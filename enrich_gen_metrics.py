3eaad02f35a0de4143eb2cee2b06c8cdc16fa791
Roman Vashurin <rvashurin@gmail.com>
2025-03-29 20:40:42 +0400

    Separate scripts for gen metric and stat enrichment

--- enrich_gen_metrics.py
+++ enrich_gen_metrics.py
@@ -20,7 +20,8 @@ def parse_args():
 
     parser.add_argument('--model', type=str, default='falcon7b')
     parser.add_argument('--datasets', nargs='+', default=['trivia', 'mmlu', 'coqa', 'gsm8k_cot', 'xsum', 'wmt14_fren', 'wmt19_deen'])
-    parser.add_argument('--deberta_device', type=str, default='cuda:0')
+    parser.add_argument('--mt5_device', type=str, default='cuda:0')
+    parser.add_argument('--api_key', type=str, default='')
     parser.add_argument('--script_dir', type=str, default='/workspace/mans')
     parser.add_argument('--out_dir', type=str, default='/workspace/mans_enriched')
 
@@ -51,15 +52,46 @@ def main(args):
     script_dir = args.script_dir
     out_dir = args.out_dir
 
-    nli_model = Deberta(batch_size=1, device=args.deberta_device)
-
     stat_calculators = [
         FirstSampleCalculator(),
         BestSampleCalculator(),
-        SemanticMatrixCalculator(nli_model=nli_model),
-        GreedySemanticMatrixCalculator(nli_model=nli_model),
-        ConcatSemanticMatrixCalculator(nli_model=nli_model),
-        ConcatGreedySemanticMatrixCalculator(nli_model=nli_model),
+    ]
+
+    model_name_or_path="google/metricx-24-hybrid-large-v2p6"
+    tokenizer_name="google/mt5-large"
+    device = torch.device(args.mt5_device)
+    model_xmetric = MT5ForRegression.from_pretrained(model_name_or_path)
+    model_xmetric.to(device)
+    model_xmetric.eval()
+    tokenizer_xmetric = AutoTokenizer.from_pretrained(
+        tokenizer_name if tokenizer_name else model_name_or_path
+    )
+
+    gen_metrics_wmt = [
+        XMetric(model=model_xmetric,
+                tokenizer=tokenizer_xmetric,
+                source_ignore_regex="(?s).*Original:\n(.*?)\nTranslation:\n"),
+        XMetric(model=model_xmetric,
+                tokenizer=tokenizer_xmetric,
+                source_ignore_regex="(?s).*Original:\n(.*?)\nTranslation:\n",
+                sample=True),
+        XMetric(model=model_xmetric,
+                tokenizer=tokenizer_xmetric,
+                source_ignore_regex="(?s).*Original:\n(.*?)\nTranslation:\n",
+                sample=True,
+                sample_strategy='Best'),
+        XMetric(model=model_xmetric,
+                tokenizer=tokenizer_xmetric,
+                source_ignore_regex="(?s).*Original:\n(.*?)\nTranslation:\n",
+                sample=True,
+                sample_strategy='BestNormalized'),
+    ]
+
+    gen_metrics_qa = [
+        GptAccuracyMetric(api_key=args.api_key),
+        GptAccuracyMetric(api_key=args.api_key, sample=True),
+        GptAccuracyMetric(api_key=args.api_key, sample=True, sample_strategy='Best'),
+        GptAccuracyMetric(api_key=args.api_key, sample=True, sample_strategy='BestNormalized'),
     ]
 
 
@@ -86,6 +118,15 @@ def main(args):
                 stats.update(values)
 
             man.stats = stats
+
+            if 'wmt' in dataset:
+                for gen_metric in gen_metrics_wmt:
+                    values = gen_metric(stats=man.stats, target_texts=stats['target_texts'])
+                    man.gen_metrics[('sequence', str(gen_metric))] = values
+            elif dataset in ['coqa', 'gsm8k_cot', 'trivia', 'mmlu']:
+                for gen_metric in gen_metrics_qa:
+                    values = gen_metric(stats=man.stats, target_texts=stats['target_texts'])
+                    man.gen_metrics[('sequence', str(gen_metric))] = values
 
             pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
             man.save_path = os.path.join(out_dir, f"{model}_{dataset}.man")
